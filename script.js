52923bfd3efa5c4f63da3f679b413248b75a37ac
Patrick Morais <ppternunes@gmail.com>
2021-03-12 13:26:35 -0300

    n sei q q ta pegando

--- script.js
+++ script.js
@@ -21,14 +21,13 @@ function createLetter() {
 }
 
 function generateClasses() {
-  const indexRandom1 = Math.floor(Math.random() * 2);
-  const indexRandom2 = Math.floor(Math.random() * 2);
-  const indexRandom3 = Math.floor(Math.random() * 1);
-  const indexRandom4 = Math.floor(Math.random() * 1);
-
   const spans = document.querySelectorAll('#carta-gerada span');
 
   for (let i = 0; i < spans.length; i += 1) {
+    const indexRandom1 = Math.floor(Math.random() * 3);
+    const indexRandom2 = Math.floor(Math.random() * 3);
+    const indexRandom3 = Math.floor(Math.random() * 2);
+    const indexRandom4 = Math.floor(Math.random() * 2);
     spans[i].classList.add(styleGroup[indexRandom1]);
     spans[i].classList.add(weightGroup[indexRandom2]);
     spans[i].classList.add(rotate[indexRandom3]);
