081b614c98a7f068da853efcbf3a277579debb98
Lucas André <lucas.cancado89@gmail.com>
2021-03-13 18:39:14 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const letterP = document.getElementById('carta-gerada');
+const tagP = document.getElementById('criar-carta');
+
 function clearAll() {
   while (letterP.firstChild) {
     letterP.removeChild(letterP.firstChild);
   }
 }
 
-document.getElementById('criar-carta').addEventListener('click', clearAll);
+tagP.addEventListener('click', clearAll);
 
 function generateLetter() {
   const str = document.getElementById('carta-texto').value;
@@ -20,4 +22,22 @@ function generateLetter() {
   }
 }
 
-document.getElementById('criar-carta').addEventListener('click', generateLetter);
+tagP.addEventListener('click', generateLetter);
+
+const styleClass = ['newspaper', 'magazine1', 'magazine2'];
+const sizeClass = ['medium', 'big', 'reallybig'];
+const rotationClass = ['rotateleft', 'rotateright'];
+const inclinationclass = ['skewleft', 'skewright'];
+const words = document.getElementsByTagName('span');
+
+function stylingLetter() {
+  for (let index = 0; index < words.length; index += 1) {
+    const styleNum = Math.floor(Math.random() * 3);
+    const sizeNum = Math.floor(Math.random() * 3);
+    const rotationNum = Math.floor(Math.random() * 2);
+    const inclinationNum = Math.floor(Math.random() * 2);
+    words[index].className = `${styleClass[styleNum]} ${sizeClass[sizeNum]} ${rotationClass[rotationNum]} ${inclinationclass[inclinationNum]}`;
+  }
+}
+
+tagP.addEventListener('click', stylingLetter);
