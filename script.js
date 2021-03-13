41cec22badf30876d231154a9b86d7b2e7836992
Lucas André <lucas.cancado89@gmail.com>
2021-03-13 18:06:49 -0300

    Requisito 6 ao 15

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const letterP = document.getElementById('carta-gerada');
+function clearAll() {
+  while (letterP.firstChild) {
+    letterP.removeChild(letterP.firstChild);
+  }
+}
+
+document.getElementById('criar-carta').addEventListener('click', clearAll);
+
 function generateLetter() {
   const str = document.getElementById('carta-texto').value;
   const arr = str.split(' ');
-  for (let index = 0; index <= arr.length; index += 1) {
+  for (let index = 0; index < arr.length; index += 1) {
     const tagSpan = document.createElement('span');
     tagSpan.innerText = arr[index];
-    document.getElementById('carta-gerada').appendChild(tagSpan);
+    letterP.appendChild(tagSpan);
   }
   if (str === '' || str === ' ') {
-    document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta';
+    letterP.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
