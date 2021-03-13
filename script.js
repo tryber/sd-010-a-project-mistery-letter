9b5f3463440c800f8e7554386763f1a52ff6aa2a
Lucas André <lucas.cancado89@gmail.com>
2021-03-13 18:59:01 -0300

    Requisito 17 e 18

--- script.js
+++ script.js
@@ -24,20 +24,26 @@ function generateLetter() {
 
 tagP.addEventListener('click', generateLetter);
 
-const styleClass = ['newspaper', 'magazine1', 'magazine2'];
-const sizeClass = ['medium', 'big', 'reallybig'];
-const rotationClass = ['rotateleft', 'rotateright'];
-const inclinationclass = ['skewleft', 'skewright'];
+const sC = ['newspaper', 'magazine1', 'magazine2'];
+const siC = ['medium', 'big', 'reallybig'];
+const rC = ['rotateleft', 'rotateright'];
+const iC = ['skewleft', 'skewright'];
 const words = document.getElementsByTagName('span');
 
 function stylingLetter() {
   for (let index = 0; index < words.length; index += 1) {
-    const styleNum = Math.floor(Math.random() * 3);
-    const sizeNum = Math.floor(Math.random() * 3);
-    const rotationNum = Math.floor(Math.random() * 2);
-    const inclinationNum = Math.floor(Math.random() * 2);
-    words[index].className = `${styleClass[styleNum]} ${sizeClass[sizeNum]} ${rotationClass[rotationNum]} ${inclinationclass[inclinationNum]}`;
+    const sN = Math.floor(Math.random() * 3);
+    const siN = Math.floor(Math.random() * 3);
+    const rN = Math.floor(Math.random() * 2);
+    const iN = Math.floor(Math.random() * 2);
+    words[index].className = `${sC[sN]} ${siC[siN]} ${rC[rN]} ${iC[iN]}`;
   }
 }
 
 tagP.addEventListener('click', stylingLetter);
+
+function wordCount() {
+  document.getElementById('carta-contador').innerText = words.length;
+}
+
+tagP.addEventListener('click', wordCount);
