f5e2e62c4ab60c0c72e4b8592b90be4927b468d4
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-14 14:51:36 -0300

    quarto e quinto requisito

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ getInput.addEventListener('keyup', (event) => {
 const getBtn = document.getElementById('criar-carta');
 getBtn.addEventListener('click', () => {
   const getLetter = document.getElementById('carta-texto').value;
+  if (getLetter.trim().length === 0) {
+    document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.'
+  }
   const arrayLetter = getLetter.split(' ');
   for (let index = 0; index < arrayLetter.length; index += 1) {
     const makeSpan = document.createElement('span');
-    document.getElementById('container').appendChild(makeSpan);
+    document.getElementById('carta-gerada').appendChild(makeSpan);
     document.querySelectorAll('span')[index].innerText = `${arrayLetter[index]} `;
   }
 });
