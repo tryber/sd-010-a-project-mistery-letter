a615416485193609f3f3f6c693f01e5e5712895a
Gabriel Ferreira <glucas709@gmail.com>
2021-03-14 23:44:26 -0300

    Requisito 03 - Gerando carta ao clicar no botão

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnGenerateLetter = document.getElementById('criar-carta');
+
+function generateWords() {
+  const textInput = document.getElementById('carta-texto').value;
+  const words = textInput.split(' ');
+  return words;
+}
+
+function generateLetter() {
+  const misteryousLetter = document.getElementById('carta-gerada');
+  const words = generateWords();
+  for (let i = 0; i < words.length; i += 1) {
+    const word = document.createElement('span');
+    word.innerHTML = words[i];
+    misteryousLetter.appendChild(word);
+  }
+}
+
+btnGenerateLetter.addEventListener('click', generateLetter);
