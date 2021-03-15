2b33faffd9e28218fcfe04b67c61946b176d696e
Gabriel Ferreira <glucas709@gmail.com>
2021-03-15 00:56:05 -0300

    Requisito 05 - Verificando se está vazio ou somente espaços em branco

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btnGenerateLetter = document.getElementById('criar-carta');
+const inputText = document.getElementById('carta-texto');
+const misteryousLetter = document.getElementById('carta-gerada');
 
 function generateWords() {
   const textInput = document.getElementById('carta-texto').value;
@@ -6,12 +8,18 @@ function generateWords() {
   return words;
 }
 
+function checkEmpty() {
+  if (inputText.value === '' || !inputText.value.replace(/\s/g, '').length) {
+    misteryousLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+}
+
 function generateLetter() {
-  const misteryousLetter = document.getElementById('carta-gerada');
   const words = generateWords();
   if (misteryousLetter.innerText !== '') {
     misteryousLetter.innerHTML = '';
   }
+  checkEmpty();
   for (let i = 0; i < words.length; i += 1) {
     const word = document.createElement('span');
     word.innerHTML = `${words[i]}`;
