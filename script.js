b5060b35cae83b72da34d11b8ce46a1f5edcb8b7
Gabriel Ferreira <glucas709@gmail.com>
2021-03-18 13:53:23 -0300

    Requisito 16 - Gerando classes aleatóriamente

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const btnGenerateLetter = document.getElementById('criar-carta');
 const inputText = document.getElementById('carta-texto');
 const misteryousLetter = document.getElementById('carta-gerada');
 
+const stylization = {
+  styleGroup: ['newspaper', 'magazine1', 'magazine2'],
+  sizeGroup: ['medium', 'big', 'reallybig'],
+  rotateGroup: ['rotateleft', 'rotateright'],
+  inclinateGroup: ['skewleft', 'skewright'],
+};
+
 function generateWords() {
   const textInput = document.getElementById('carta-texto').value;
   const words = textInput.split(' ');
@@ -11,6 +18,86 @@ function generateWords() {
 function checkEmpty() {
   if (inputText.value === '' || !inputText.value.replace(/\s/g, '').length) {
     misteryousLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+}
+
+function getRandomArray(array) {
+  let random;
+  let n;
+  const arrayIndex = array;
+
+  for (let i = arrayIndex.length - 1; i; i -= 1) {
+    random = Math.round(Math.random() * i);
+    n = arrayIndex[i];
+    arrayIndex[i] = arrayIndex[random];
+    arrayIndex[random] = n;
+  }
+  return arrayIndex;
+}
+
+// function getIndex(array) {
+//   const indexes = [];
+
+//   for (let i = 0; i < array.length; i += 1) {
+//     indexes.push(i);
+//   }
+//   return indexes;
+// }
+
+function generateGroupArray(quant) {
+  const numArray = [];
+
+  for (let i = 0; numArray.length !== quant; i += 1) {
+    const num = Math.floor(Math.random() * quant);
+    if (numArray.indexOf(num) === -1) {
+      numArray.push(num);
+    }
+  }
+  return numArray;
+}
+
+function generateClassArray(quant, maxValue) {
+  const numArray = [];
+
+  for (let i = 0; i < quant; i += 1) {
+    const num = Math.floor(Math.random() * maxValue);
+    if (num < maxValue && numArray !== '') {
+      numArray.push(num);
+    }
+  }
+  return numArray;
+}
+
+function getStylization() {
+  let numClasses = Math.floor((Math.random() * 4) + 1);
+  const classes = [];
+
+  numClasses = numClasses < 2 ? numClasses += 1 : numClasses;
+  console.log('Num classes' + numClasses);
+
+  const numGroupArray = generateGroupArray(numClasses);
+
+  for (let j = 0; j < numClasses; j += 1) {
+    const groupSelected = Object.values(stylization)[numGroupArray[j]];
+    const numClassArray = Math.floor(Math.random() * groupSelected.length);
+    classes.push(getRandomArray(groupSelected)[numClassArray]);
+    console.log('ESSE É O GRUPO' + groupSelected);
+    console.log('AQUI SÃO AS CLASSES' + numClassArray);
+  }
+  console.log('As classess >>>>>>>>>>>>>>>>>>> ' + classes);
+  return classes;
+}
+
+
+function setStylization() {
+  // const span = document.getElementsByName('span');
+
+  for (let i = 0; i < misteryousLetter.children.length; i += 1) {
+    const classes = getStylization();
+    for (let j = 0; j < classes.length; j += 1) {
+      misteryousLetter.children[i].classList.add(classes[j]);
+      console.log(misteryousLetter.children[i].classList);
+    }
   }
 }
 
@@ -25,6 +112,7 @@ function generateLetter() {
     word.innerHTML = `${words[i]}`;
     misteryousLetter.appendChild(word);
   }
+  setStylization();
 }
 
 btnGenerateLetter.addEventListener('click', generateLetter);
