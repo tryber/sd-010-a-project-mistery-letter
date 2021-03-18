75dcb4786bf2013b92dd8afaf9e3297765074089
Gabriel Ferreira <glucas709@gmail.com>
2021-03-18 15:30:51 -0300

    Requisito 18 - Contador de palavras

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnGenerateLetter = document.getElementById('criar-carta');
 const inputText = document.getElementById('carta-texto');
 const misteryousLetter = document.getElementById('carta-gerada');
+const count = document.getElementById('carta-contador');
 
 const stylization = {
   styleGroup: ['newspaper', 'magazine1', 'magazine2'],
@@ -11,14 +12,17 @@ const stylization = {
 
 function generateWords() {
   const textInput = document.getElementById('carta-texto').value;
-  const words = textInput.split(' ');
-  return words;
+  if (textInput.length > 0) {
+    const words = textInput.split(' ');
+    return words;
+  }
 }
 
 function checkEmpty() {
   if (inputText.value === '' || !inputText.value.replace(/\s/g, '').length) {
-    misteryousLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    return true;
   }
+  return false;
 }
 
 function getRandomArray(array) {
@@ -35,15 +39,6 @@ function getRandomArray(array) {
   return arrayIndex;
 }
 
-// function getIndex(array) {
-//   const indexes = [];
-
-//   for (let i = 0; i < array.length; i += 1) {
-//     indexes.push(i);
-//   }
-//   return indexes;
-// }
-
 function generateGroupArray(quant) {
   const numArray = [];
 
@@ -56,24 +51,11 @@ function generateGroupArray(quant) {
   return numArray;
 }
 
-function generateClassArray(quant, maxValue) {
-  const numArray = [];
-
-  for (let i = 0; i < quant; i += 1) {
-    const num = Math.floor(Math.random() * maxValue);
-    if (num < maxValue && numArray !== '') {
-      numArray.push(num);
-    }
-  }
-  return numArray;
-}
-
 function getStylization() {
   let numClasses = Math.floor((Math.random() * 4) + 1);
   const classes = [];
 
   numClasses = numClasses < 2 ? numClasses += 1 : numClasses;
-  console.log('Num classes' + numClasses);
 
   const numGroupArray = generateGroupArray(numClasses);
 
@@ -81,38 +63,56 @@ function getStylization() {
     const groupSelected = Object.values(stylization)[numGroupArray[j]];
     const numClassArray = Math.floor(Math.random() * groupSelected.length);
     classes.push(getRandomArray(groupSelected)[numClassArray]);
-    console.log('ESSE É O GRUPO' + groupSelected);
-    console.log('AQUI SÃO AS CLASSES' + numClassArray);
   }
-  console.log('As classess >>>>>>>>>>>>>>>>>>> ' + classes);
   return classes;
 }
 
+function setStylizationWord() {
+  misteryousLetter.addEventListener('click', (e) => {
+    const word = e.target;
+    const classes = getStylization();
+    word.classList = '';
+    for (let i = 0; i < classes.length; i += 1) {
+      word.classList.add(classes[i]);
+    }
+  });
+}
 
 function setStylization() {
-  // const span = document.getElementsByName('span');
-
   for (let i = 0; i < misteryousLetter.children.length; i += 1) {
     const classes = getStylization();
     for (let j = 0; j < classes.length; j += 1) {
       misteryousLetter.children[i].classList.add(classes[j]);
-      console.log(misteryousLetter.children[i].classList);
     }
   }
 }
 
+function wordsCount() {
+  count.innerHTML = generateWords().length;
+}
+
 function generateLetter() {
   const words = generateWords();
-  if (misteryousLetter.innerText !== '') {
-    misteryousLetter.innerHTML = '';
-  }
-  checkEmpty();
   for (let i = 0; i < words.length; i += 1) {
     const word = document.createElement('span');
     word.innerHTML = `${words[i]}`;
     misteryousLetter.appendChild(word);
   }
   setStylization();
+  setStylizationWord();
+  wordsCount();
 }
 
-btnGenerateLetter.addEventListener('click', generateLetter);
+count.innerHTML = 0;
+
+btnGenerateLetter.addEventListener('click', () => {
+  const isEmpty = checkEmpty();
+  if (isEmpty === true) {
+    count.innerHTML = 0;
+    misteryousLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+  if (isEmpty === false) {
+    misteryousLetter.innerHTML = '';
+    generateLetter();
+  }
+});
