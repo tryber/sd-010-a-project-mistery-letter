c93250a8097fd10562ac35a18825631369c0419b
defauth98 <neto.daniribeiro@gmail.com>
2021-03-18 17:02:25 -0300

    Exercicio 03

---
+++ script.js
@@ -0,0 +1,20 @@
+function createLetter() {
+  const letterInputElement = document.getElementById('carta-texto');
+  const letterElement = document.getElementById('carta-gerada');
+
+  const letterContent = letterInputElement.value;
+  const letterContentArray = letterContent.split(' ');
+
+  for (let index = 0; index < letterContentArray.length; index += 1) {
+    const spanElement = document.createElement('span');
+    spanElement.innerHTML = `${letterContentArray[index]} `;
+
+    letterElement.appendChild(spanElement);
+  }
+}
+
+window.onload = () => {
+  const createLetterButtonElement = document.getElementById('criar-carta');
+
+  createLetterButtonElement.onclick = createLetter;
+};
