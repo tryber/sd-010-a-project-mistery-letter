a23e44aea1d9e36dba4a42ace4aec506c8abbc57
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-21 09:34:52 -0300

    Corrections requirement 3 and requeriment 17 Done

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
 const selectButton = document.querySelector('#criar-carta');
 const selectInput = document.querySelector('#carta-texto');
 const selectP = document.querySelector('#carta-gerada');
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+const lengths = ['medium', 'big', 'reallybig'];
+const rotates = ['rotateleft', 'rotateright'];
+const Skews = ['skewleft', 'skewright'];
 
-if (selectInput.value === '') {
-  selectP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+function randomStyle() {
+  const randomStyles = Math.floor(Math.random() * styles.length);
+  return randomStyles;
+}
+
+function addStyle() {
+  const newStyle = styles[randomStyle()];
+  return newStyle;
+}
+
+function randomLenght() {
+  const randomLenghts = Math.floor(Math.random() * lengths.length);
+  return randomLenghts;
+}
+
+function addleght() {
+  const newLenght = lengths[randomLenght()];
+  return newLenght;
+}
+
+function ramdomRotate() {
+  const randomRotates = Math.floor(Math.random() * rotates.length);
+  return randomRotates;
+}
+
+function addRotate() {
+  const newRotate = rotates[ramdomRotate()];
+  return newRotate;
+}
+
+function randomSkew() {
+  const randomSkews = Math.floor(Math.random() * Skews.length);
+  return randomSkews;
+}
+
+function addSkew() {
+  const newSkew = Skews[randomSkew()];
+  return newSkew;
 }
 
 function generateLetter() {
-  const createSpan = document.createElement('span');
-  createSpan.innerText = selectInput.value;
-  selectP.appendChild(createSpan);
+  selectP.innerHTML = '';
+  const inputSplited = selectInput.value.split(' ');
+  if (selectInput.value === '' || !selectInput.value.trim()) { // consulta do método trim() em https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    selectP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    for (let index = 0; index < inputSplited.length; index += 1) {
+      const createSpan = document.createElement('span');
+      createSpan.innerHTML = inputSplited[index];
+      createSpan.style.display = 'inline-block';
+      createSpan.classList.add(`${addStyle()}`);
+      createSpan.classList.add(`${addleght()}`);
+      createSpan.classList.add(`${addRotate()}`);
+      createSpan.classList.add(`${addSkew()}`);
+      selectP.appendChild(createSpan);
+    }
+  }
 }
 
 selectButton.addEventListener('click', generateLetter);
