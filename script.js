e39c90ecd00d46ae49dc6651b7939e7014d11be2
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-20 23:12:47 -0300

    Requirement 4 and 5 Done

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const selectButton = document.querySelector('#criar-carta');
 const selectInput = document.querySelector('#carta-texto');
 const selectP = document.querySelector('#carta-gerada');
 
+if (selectInput.value === '') {
+  selectP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+}
+
 function generateLetter() {
   const createSpan = document.createElement('span');
   createSpan.innerText = selectInput.value;
