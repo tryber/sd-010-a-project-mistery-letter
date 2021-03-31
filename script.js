5c24b93958d7418c2469ab67343dcac555046750
Orlando Flores <orlando_flores@id.uff.br>
2021-03-31 17:15:53 -0300

    requisito 18

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ const styles = ['newspaper', 'magazine1', 'magazine2'];
 const sizes = ['medium', 'big', 'reallybig'];
 const rotations = ['rotateleft', 'rotateright'];
 const inclinations = ['skewleft', 'skewright'];
-
+const count = document.querySelector('#carta-contador');
 const setClassToSpan = (element) => {
-  console.log('entrou');
   const style = styles[(Math.floor(Math.random() * styles.length))];
   const size = sizes[(Math.floor(Math.random() * sizes.length))];
   const rotation = rotations[(Math.floor(Math.random() * rotations.length))];
@@ -17,9 +16,13 @@ const setClassToSpan = (element) => {
 
 const appendSpan = (text, boolean) => {
   const span = document.createElement('span');
+  let contador = 0;
   span.innerText = text;
-  if (boolean) setClassToSpan(span);
-  letter.appendChild(span);
+  if (boolean) {
+    setClassToSpan(span);
+    contador += 1;
+  }
+    letter.appendChild(span);
 };
 
 btnText.addEventListener('click', () => {
@@ -29,4 +32,6 @@ btnText.addEventListener('click', () => {
   if (text.length === 0) appendSpan('Por favor, digite o conteúdo da carta.', false);
   else textArray = text.split(' ');
   textArray.forEach((element) => appendSpan(element, true));
+  const spans = document.querySelectorAll('span');
+  count.innerText = spans.length;
 });
