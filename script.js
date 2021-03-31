bf12c5b59e8534ca93fe9cc8e84a9f68fd11a808
Orlando Flores <orlando_flores@id.uff.br>
2021-03-31 11:45:13 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const inputText = document.querySelector('#carta-texto');
 const btnText = document.querySelector('#criar-carta');
 const letter = document.querySelector('#carta-gerada');
+
+const appendSpan = (text) => {
+  const span = document.createElement('span');
+  span.innerText = text;
+  letter.appendChild(span);
+};
+
 btnText.addEventListener('click', () => {
   const text = inputText.value;
+  if (text.length === 0) appendSpan('Por favor, digite o conteúdo da carta.');
+  else letter.innerText = '';
   const textArray = text.split(' ');
-  textArray.forEach((element) => {
-    const span = document.createElement('span');
-    span.innerText = element;
-    letter.appendChild(span);
-  });
+  textArray.forEach((element) => appendSpan(element));
 });
