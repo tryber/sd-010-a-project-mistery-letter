97e61964d759dc844ebdaa0c73537a29ebb3b81d
Orlando Flores <orlando_flores@id.uff.br>
2021-03-31 16:14:56 -0300

    requisito 16

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const inputText = document.querySelector('#carta-texto');
 const btnText = document.querySelector('#criar-carta');
 const letter = document.querySelector('#carta-gerada');
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+const sizes = ['medium', 'big', 'reallybig'];
+const rotations = ['rotateleft', 'rotateright'];
+const inclinations = ['skewleft', 'skewright'];
 
-const appendSpan = (text) => {
+const setClassToSpan = (element) => {
+  console.log('entrou');
+  const style = styles[(Math.floor(Math.random() * styles.length))];
+  const size = sizes[(Math.floor(Math.random() * sizes.length))];
+  const rotation = rotations[(Math.floor(Math.random() * rotations.length))];
+  const inclination = inclinations[(Math.floor(Math.random() * inclinations.length))];
+  element.classList.add(style, size, rotation, inclination);
+};
+
+const appendSpan = (text, boolean) => {
   const span = document.createElement('span');
   span.innerText = text;
+  if (boolean) setClassToSpan(span);
   letter.appendChild(span);
 };
 
 btnText.addEventListener('click', () => {
   const text = inputText.value;
-  if (text.length === 0) appendSpan('Por favor, digite o conteúdo da carta.');
-  else letter.innerText = '';
-  const textArray = text.split(' ');
-  textArray.forEach((element) => appendSpan(element));
+  letter.innerText = '';
+  let textArray = [];
+  if (text.length === 0) appendSpan('Por favor, digite o conteúdo da carta.', false);
+  else textArray = text.split(' ');
+  textArray.forEach((element) => appendSpan(element, true));
 });
