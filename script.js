7fdd4e723608347e07bcfdae8936fd9d253c1ff0
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-04-27 20:37:43 -0300

    decimo setimo requisito

--- script.js
+++ script.js
@@ -10,13 +10,18 @@ getInput.addEventListener('keyup', (event) => {
   getInput.innerText += phrase;
 });
 
+function countWords(inputText) {
+  const count = inputText.split(' ').length;
+  document.getElementById('carta-contador').innerText = count;
+}
+
 const getBtn = document.getElementById('criar-carta');
 getBtn.addEventListener('click', () => {
   const getLetter = document.getElementById('carta-texto').value;
   if (getLetter.trim().length === 0) {
     document.getElementById('carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
   }
-  const arrayLetter = getLetter.split(' ');
+  const arrayLetter = getLetter.trim().split(' ');
   for (let index = 0; index < arrayLetter.length; index += 1) {
     const makeSpan = document.createElement('span');
     document.getElementById('carta-gerada').appendChild(makeSpan);
@@ -25,9 +30,5 @@ getBtn.addEventListener('click', () => {
     word.classList.add(groupClasses[Math.ceil(Math.random() * 9) - 1]);
     word.innerText = `${arrayLetter[index]} `;
   }
+  countWords(getLetter);
 });
-
-// let getSpan = document.querySelectorAll('span');
-// for (let index = 0; index < getSpan.length; index += 1) {
-//   getSpan[index].classList.add(groupSize[Math.ceil(Math.random() * 3)])
-// }
